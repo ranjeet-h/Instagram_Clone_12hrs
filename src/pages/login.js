6f94af2f9b15228c45bdab8bd689c013889f2b36
Ranjeet Harishchandre <ranjeetharishchandre@gmail.com>
2021-04-11 22:01:44 +0530

    Start of project at 01.55:00 on 110421 1001pm

--- src/pages/login.js
+++ src/pages/login.js
@@ -17,7 +17,7 @@ export default function Login() {
   useEffect(() => {
     document.title = "Login-Instagram";
   }, []);
-  
+
   return (
     <>
       <div className="container flex mx-auto max-w-screen-md items-center h-screen">
@@ -29,7 +29,30 @@ export default function Login() {
           />
         </div>
         <div className="flex flex-col w-2/5">
-          <p>I will be the form</p>
+          <h1 className="flex justify-center w-full">
+            <img
+              className="mt-2 w-6/12 mb-4"
+              src="/images/logo.png"
+              alt="instagram"
+            />
+          </h1>
+          {error && <p className="mb-4 text-xs test-red-primary">{error}</p>}
+          <form onSubmit={handleLogin} method="post">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2 outline-none"
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2 outline-none"
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+          </form>
         </div>
       </div>
     </>
