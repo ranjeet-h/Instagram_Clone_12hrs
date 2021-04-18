a727c1b27ebacce55a600c5546f6ef4dba029316
Ranjeet Harishchandre <ranjeetharishchandre@gmail.com>
2021-04-18 18:45:21 +0530

    Start of project at 04.52:00 on 180421 0644pm

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from "react-router-dom"
+import Skeleton from "react-loading-skeleton"
 
-export default function User() {
-    return (
-        <div>
-            <p>I am a user</p>
-        </div>
-    )
+const User = ({ username, fullName }) =>
+    !username || !fullName ? (<Skeleton count={1} height={61} />)
+        :
+        (<Link to={`/p/${username}`} className="grid grid-cols-4 gap-4 mb-6 items-center">
+            <div className="flex item-center justify-between col-span-1">
+                <img className="w-10 flex mr-3" src="/images/avatars/avatar.png" alt="" />
+            </div>
+            <div className="col-span-3">
+                <p className="font-bold text-sm" >{username}</p>
+                <p className="text-sm">{fullName}</p>
+            </div>
+        </Link>)
+
+
+
+export default memo(User);
+
+User.propTypes = {
+    username: PropTypes.string,
+    fullName: PropTypes.string
 }
--- src/components/timeline.js
+++ src/components/timeline.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Timeline() {
     return (
-        <div>
+        <div className="container col-span-2">
             <p className="">I am a Timeline</p>
         </div>
     )
